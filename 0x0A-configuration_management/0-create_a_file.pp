911e7d9465037c4a4339d76794941fdd3a00bdf2
beckyshaz <sharonsakah8@gmail.com>
2024-06-28 07:37:56 -0700

    puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 #create a file in /tmp:
 
 file{'/tmp/school':
-ensure => '/tmp/school',
- content => 'I love Puppet',
-mode => '0744',
-group => 'www-data',
-owner => 'www-data',
+ensure  => 'file',
+content => 'I love Puppet',
+mode    => '0744',
+group   => 'www-data',
+owner   => 'www-data',
 }
