b5a917e3e3c3783ea47bfbaa047890aa1567c625
beckyshaz <sharonsakah8@gmail.com>
2024-06-29 06:49:01 -0700

    installing package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,13 @@
-#Install flask from pip3:
+# Install flask from pip3:
 
 package{'flask':
 ensure   => '2.1.0',
+name     => 'flask',
+provider => 'pip3',
+}
+
+package{'werkzeug':
+ensure   => '2.1.1',
+name     => 'werkzeug',
 provider => 'pip3',
 }
