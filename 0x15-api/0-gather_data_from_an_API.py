f0672e7d19d443b8dea00667bc56d2c193ef3c90
beckyshaz <sharonsakah8@gmail.com>
2024-08-08 21:31:17 -0700

    loop chec

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -26,4 +26,4 @@ if __name__ == "__main__":
 
     for todo in todos:
         if todo.get("completed") is True:
-            print(f"\t{todo.get('title')}")
+            print(f"\t {todo.get('title')}")
