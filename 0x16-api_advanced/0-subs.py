628ff40f9c0055d9bfbc14df0e8ee92292cb8272
beckyshaz <sharonsakah8@gmail.com>
2024-08-13 07:10:16 -0700

    redit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,6 +6,7 @@ import requests as req
 
 
 def number_of_subscribers(subreddit):
+    """requsting reddit api"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
     resp = req.get(url, headers=headers, allow_redirects=False)
