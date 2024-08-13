05e55945c2f49d1ed9c662359d9677f9e276d796
beckyshaz <sharonsakah8@gmail.com>
2024-08-13 07:07:58 -0700

    redit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""queries the Reddit API and returns the number of subscribers"""
+
+
+import requests as req
+
+
+def number_of_subscribers(subreddit):
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    resp = req.get(url, headers=headers, allow_redirects=False)
+    if resp.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
+        return 0
