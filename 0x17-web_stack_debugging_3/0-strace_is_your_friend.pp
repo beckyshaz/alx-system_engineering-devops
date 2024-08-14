70d4303648427c0502274cc07c6b0094ce02eb44
beckyshaz <sharonsakah8@gmail.com>
2024-08-14 09:34:18 -0700

    fixing wordpress

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Fixxing phpp extion with php
 
-exec{"fixing-wordpress":
+exec{'fixing-wordpress':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/usr/local/bin/ :/bin'
 }
