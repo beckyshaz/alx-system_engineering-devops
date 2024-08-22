1af6b81d3cfce6f632a37d2e56a2351ead5d998d
beckyshaz <sharonsakah8@gmail.com>
2024-08-22 05:02:21 -0700

    changing number of open files in nginx

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,11 @@
+# Enable holberton user to open files without error
+
+exec {'increase-soft-file-limit-holberton-user':
+  command => 'sed -i "/^holberton soft/s/5/70000/g" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/',
+}
+
+exec {'increase-hard-file-limit-holberton-user':
+  command => 'sed -i "/^holberton hard/s/4/70000/g" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
